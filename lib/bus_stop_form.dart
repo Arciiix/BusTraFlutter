1cfd3ad2e630b497ccd86b97cd9eb5a32a38b190
Arciiix <artur.nowak100@gmail.com>
2021-12-01 00:03:33 +0100

    Paste latitude and longitude

--- lib/bus_stop_form.dart
+++ lib/bus_stop_form.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class BusStopForm extends StatefulWidget {
   @override
@@ -7,6 +8,8 @@ class BusStopForm extends StatefulWidget {
 
 class _BusStopFormState extends State<BusStopForm> {
   final bool isEditing = false;
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _destinationBusStopLatitudeController =
@@ -18,6 +21,69 @@ class _BusStopFormState extends State<BusStopForm> {
   final TextEditingController _previousBusStopLongitudeController =
       TextEditingController();
 
+  void showSnackBar(String message) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text(message),
+    ));
+  }
+
+  Future<String?> getClipboardData() async {
+    ClipboardData? cdata = await Clipboard.getData(Clipboard.kTextPlain);
+    if (cdata == null || cdata.text == null) {
+      return null;
+    } else {
+      return cdata.text;
+    }
+  }
+
+  Future<String?> pasteCoordinatesFromClipboard() async {
+    String? clipboardContent = await getClipboardData();
+    if (clipboardContent == null) {
+    } else {
+      //Replace commas with dots for compatibility
+      clipboardContent = clipboardContent.replaceAll(",", ".");
+      return clipboardContent;
+    }
+  }
+
+  String? validateLatitude(String? value) {
+    if (value == null || value.isEmpty) {
+      return "Wartość nie może być pusta.";
+    }
+    final RegExp latitudeRegExp =
+        RegExp(r"^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?)$");
+
+    if (latitudeRegExp.hasMatch(value)) {
+      return null;
+    } else {
+      return "Niepoprawna wartość";
+    }
+  }
+
+  String? validateLongitude(String? value) {
+    if (value == null || value.isEmpty) {
+      return "Wartość nie może być pusta.";
+    }
+    final RegExp longitudeRegExp =
+        RegExp(r"^[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?)$");
+
+    if (longitudeRegExp.hasMatch(value)) {
+      return null;
+    } else {
+      return "Niepoprawna wartość";
+    }
+  }
+
+  void handleSave() {
+    if (validateWholeForm()) {
+      print("DEV - SAVE");
+    }
+  }
+
+  bool validateWholeForm() {
+    return _formKey.currentState!.validate() ? true : false;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,101 +93,174 @@ class _BusStopFormState extends State<BusStopForm> {
             IconButton(
               icon: const Icon(Icons.save),
               tooltip: "Zapisz",
-              onPressed: () => print("DEV - save bus stop form"),
+              onPressed: handleSave,
             )
           ],
         ),
         body: Padding(
-          padding: const EdgeInsets.all(5),
-          child: SingleChildScrollView(
-              child: Column(children: [
-            Padding(
-              padding: const EdgeInsets.all(5),
-              child: TextField(
-                  controller: _nameController,
-                  decoration: InputDecoration(
-                    labelText: "Nazwa",
-                  )),
-            ),
-            Padding(
-                padding: const EdgeInsets.all(10),
-                child: Column(
-                  children: [
-                    const Text(
-                      "Przystanek docelowy",
-                      style: TextStyle(fontSize: 32),
-                      textAlign: TextAlign.left,
-                    ),
-                    TextField(
-                        controller: _destinationBusStopLatitudeController,
-                        keyboardType: TextInputType.number,
-                        decoration: InputDecoration(
-                            labelText: "Szerokość geograficzna",
-                            suffixIcon: IconButton(
-                              icon: const Icon(Icons.paste),
-                              onPressed: () => print("PASTE 1.1"),
-                            ))),
-                    TextField(
-                        controller: _destinationBusStopLongitudeController,
-                        keyboardType: TextInputType.number,
-                        decoration: InputDecoration(
-                            labelText: "Długość geograficzna",
-                            suffixIcon: IconButton(
-                              icon: const Icon(Icons.paste),
-                              onPressed: () => print("PASTE 1.2"),
-                            ))),
-                    Padding(
-                        padding: const EdgeInsets.all(5),
-                        child: ElevatedButton(
-                            onPressed: () => print("PASTE FULL 1"),
-                            child: Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Icon(Icons.paste),
-                                  Text("Wklej koordynaty")
-                                ])))
-                  ],
-                )),
-            Padding(
-                padding: const EdgeInsets.all(10),
-                child: Column(
-                  children: [
-                    const Text(
-                      "Przystanek poprzedzający",
-                      style: TextStyle(fontSize: 32),
-                      textAlign: TextAlign.left,
-                    ),
-                    TextField(
-                        controller: _previousBusStopLatitudeController,
-                        keyboardType: TextInputType.number,
-                        decoration: InputDecoration(
-                            labelText: "Szerokość geograficzna",
-                            suffixIcon: IconButton(
-                              icon: const Icon(Icons.paste),
-                              onPressed: () => print("PASTE 2.1"),
-                            ))),
-                    TextField(
-                        controller: _previousBusStopLongitudeController,
-                        keyboardType: TextInputType.number,
-                        decoration: InputDecoration(
-                            labelText: "Długość geograficzna",
-                            suffixIcon: IconButton(
-                              icon: const Icon(Icons.paste),
-                              onPressed: () => print("PASTE 2.2"),
-                            ))),
-                    Padding(
-                        padding: const EdgeInsets.all(5),
-                        child: ElevatedButton(
-                            onPressed: () => print("PASTE FULL 2"),
-                            child: Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Icon(Icons.paste),
-                                  Text("Wklej koordynaty")
-                                ])))
-                  ],
-                )),
-          ])),
-        ));
+            padding: const EdgeInsets.all(5),
+            child: Form(
+              key: _formKey,
+              child: SingleChildScrollView(
+                  child: Column(children: [
+                Padding(
+                  padding: const EdgeInsets.all(5),
+                  child: TextFormField(
+                      controller: _nameController,
+                      decoration: InputDecoration(
+                        labelText: "Nazwa",
+                      ),
+                      validator: (val) {
+                        return val == null || val.isEmpty
+                            ? "Nazwa nie może być pusta"
+                            : null;
+                      }),
+                ),
+                Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Column(
+                      children: [
+                        const Text(
+                          "Przystanek docelowy",
+                          style: TextStyle(fontSize: 32),
+                          textAlign: TextAlign.left,
+                        ),
+                        TextFormField(
+                          controller: _destinationBusStopLatitudeController,
+                          keyboardType: TextInputType.number,
+                          decoration: InputDecoration(
+                              labelText: "Szerokość geograficzna",
+                              suffixIcon: IconButton(
+                                icon: const Icon(Icons.paste),
+                                onPressed: () async {
+                                  String? content =
+                                      await pasteCoordinatesFromClipboard();
+                                  if (content != null) {
+                                    setState(() {
+                                      _destinationBusStopLatitudeController
+                                          .text = content;
+                                    });
+                                    if (validateLatitude(content) != null) {
+                                      showSnackBar(
+                                          "Niepoprawna szerokość geograficzna!");
+                                    }
+                                  }
+                                },
+                              )),
+                          validator: (value) {
+                            return validateLatitude(value);
+                          },
+                        ),
+                        TextFormField(
+                            controller: _destinationBusStopLongitudeController,
+                            keyboardType: TextInputType.number,
+                            decoration: InputDecoration(
+                                labelText: "Długość geograficzna",
+                                suffixIcon: IconButton(
+                                  icon: const Icon(Icons.paste),
+                                  onPressed: () async {
+                                    String? content =
+                                        await pasteCoordinatesFromClipboard();
+                                    if (content != null) {
+                                      setState(() {
+                                        _destinationBusStopLongitudeController
+                                            .text = content;
+                                      });
+                                      if (validateLongitude(content) != null) {
+                                        showSnackBar(
+                                            "Niepoprawna długość geograficzna!");
+                                      }
+                                    }
+                                  },
+                                )),
+                            validator: (value) {
+                              return validateLongitude(value);
+                            }),
+                        Padding(
+                            padding: const EdgeInsets.all(5),
+                            child: ElevatedButton(
+                                onPressed: () => print("PASTE FULL 1"),
+                                child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Icon(Icons.paste),
+                                      Text("Wklej koordynaty")
+                                    ])))
+                      ],
+                    )),
+                Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Column(
+                      children: [
+                        const Text(
+                          "Przystanek poprzedzający",
+                          style: TextStyle(fontSize: 32),
+                          textAlign: TextAlign.left,
+                        ),
+                        TextFormField(
+                          controller: _previousBusStopLatitudeController,
+                          keyboardType: TextInputType.number,
+                          decoration: InputDecoration(
+                              labelText: "Szerokość geograficzna",
+                              suffixIcon: IconButton(
+                                icon: const Icon(Icons.paste),
+                                onPressed: () async {
+                                  String? content =
+                                      await pasteCoordinatesFromClipboard();
+                                  if (content != null) {
+                                    setState(() {
+                                      _previousBusStopLatitudeController.text =
+                                          content;
+                                    });
+                                    if (validateLatitude(content) != null) {
+                                      showSnackBar(
+                                          "Niepoprawna szerokość geograficzna!");
+                                    }
+                                  }
+                                },
+                              )),
+                          validator: (value) {
+                            return validateLatitude(value);
+                          },
+                        ),
+                        TextFormField(
+                            controller: _previousBusStopLongitudeController,
+                            keyboardType: TextInputType.number,
+                            decoration: InputDecoration(
+                                labelText: "Długość geograficzna",
+                                suffixIcon: IconButton(
+                                  icon: const Icon(Icons.paste),
+                                  onPressed: () async {
+                                    String? content =
+                                        await pasteCoordinatesFromClipboard();
+                                    if (content != null) {
+                                      setState(() {
+                                        _previousBusStopLongitudeController
+                                            .text = content;
+                                      });
+                                      if (validateLongitude(content) != null) {
+                                        showSnackBar(
+                                            "Niepoprawna długość geograficzna!");
+                                      }
+                                    }
+                                  },
+                                )),
+                            validator: (value) {
+                              return validateLongitude(value);
+                            }),
+                        Padding(
+                            padding: const EdgeInsets.all(5),
+                            child: ElevatedButton(
+                                onPressed: () => print("PASTE FULL 2"),
+                                child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Icon(Icons.paste),
+                                      Text("Wklej koordynaty")
+                                    ])))
+                      ],
+                    )),
+              ])),
+            )));
   }
 }
